d1a8d3be266bd2d09417ea89fe9edc015a1c8fc3
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2023-11-14 18:29:01 +0000

    Projects swipers

--- JS/main.js
+++ JS/main.js
@@ -28,3 +28,25 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// SWIPER PROJECTS
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24, 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    // mousewheel: true,
+    // keyboard: true,
+
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+    },
+});
