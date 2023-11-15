1c63b5aa6164a88ca6dcdaf9d6f4f18367d436b8
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2023-11-15 14:05:53 +0000

    Contact section finished

--- JS/main.js
+++ JS/main.js
@@ -50,3 +50,45 @@ let swiperProjects = new Swiper(".projects__container", {
         },
     },
 });
+
+// EMAIL JS
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+// check if the field has a value 
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        //add and remove color
+        contactMessage.classList.remove('color-blue');
+        contactMessage.classList.add('color-red');
+
+        //show message
+        contactMessage.textContent = 'Write the input fields 📝';
+    }else {
+        // serviceID - templateID - #form - publicKey(account) :all form https://dashboard.emailjs.com/admin/templates/44zxkc9/settings
+        emailjs.sendForm('service_x912muh','template_64vaf1p','#contact-form','76aRxnZ3CNIxISm9z')
+            .then(() => {
+                //show message and add color
+                contactMessage.classList.add('color-blue');
+                contactMessage.textContent = 'Message sent ✅';
+
+                //remove message after 5sec
+                setTimeout(() => {
+                    contactMessage.textContent = '';
+                }, 5000);
+            }, (error) => {
+                alert('OOPS! SOMETHING HAD FAILED...', error);
+            });
+        
+        //clean the input fields
+        contactName.value = '';
+        contactEmail.value = '';
+        contactProject.value = '';
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
